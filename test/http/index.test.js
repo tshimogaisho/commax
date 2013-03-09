45bd4fe5b22c69c007b2bd1f8e9633b8a274a6a9
gaishimo <tshimogaisho@gmail.com>
2013-03-09 17:26:10 +0900

    remove unused variables.

--- test/http/index.test.js
+++ test/http/index.test.js
@@ -3,18 +3,16 @@ var request = require('request');
 
 var server = require("../../server");
 
-var routes = require('../../routes');
-
 describe('index', function(){
   var httpServer;
   before(function(done) {
-    server.run( 3000, function(server, app){
+    server.run( 3000, function(server){
       done();
       httpServer = server;
     });
   });
   it('#show index page', function(done){
-    request.get('http://localhost:3000',  function(err, res, body){
+    request.get('http://localhost:3000',  function(err, res){
       should.not.exist(err);
       res.statusCode.should.equal(200);
       done();
